f3e861bdd2058ee569b32f1bb4dde4c1816271ce
xxMarina <marinapiresramos@gmail.com>
2025-03-05 19:50:29 -0300

    Subindo a classe estoque

---
+++ Estoque.java
@@ -0,0 +1,34 @@
+package main.java;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Estoque {
+    private List<Produto> produtos;
+
+    public Estoque() {
+        this.produtos = new ArrayList<>();
+    }
+
+    public void adicionarProduto(Produto produto) {
+        produtos.add(produto);
+    }
+
+    public void exibirEstoque() {
+        System.out.println("Estoque atual:");
+        for (Produto produto : produtos) {
+            System.out.println(produto.getDescricao() + " - Quantidade: " + produto.getQuantidade());
+        }
+    }
+
+    public void removerProduto(Produto produto) {
+        // Remocao do produto somente quando ele chegar/for 0
+        if (produto.getQuantidade() == 0) {
+            produtos.removeIf(produtoParaRemover -> produtoParaRemover.getNome().equals(produto.getNome()));
+            System.out.println(produto.getNome() + " removido com sucesso");
+        } else {
+            System.out.println("Não é possível remover " + produto.getNome() + " enquanto houver estoque.");
+        }
+    }
+
+}
+
